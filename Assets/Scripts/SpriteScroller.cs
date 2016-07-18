f10fd33676115adcfad745997d51a6431b627845
Erick Lucena Palmeira Silva <ericklps@gmail.com>
2016-07-18 04:57:35 -0300

    Add parallax effect

---
+++ Assets/Scripts/SpriteScroller.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    class SpriteScroller : MonoBehaviour
+    {
+        public float scrollSpeed = 1;
+        public float tileSizeZ = 1;
+
+        private Vector3 startPosition;
+
+        void Start()
+        {
+            startPosition = transform.position;
+        }
+
+        void Update()
+        {
+            float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSizeZ);
+            transform.position = startPosition + Vector3.left * newPosition;
+        }
+    }
+}
