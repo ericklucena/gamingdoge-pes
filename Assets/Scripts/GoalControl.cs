784ab841bba0aab035336cc0f0d7c8caa96f480e
Erick Lucena Palmeira Silva <ericklps@gmail.com>
2016-07-20 01:44:30 -0300

    Add goal detection

---
+++ Assets/Scripts/GoalControl.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace Assets.Scripts
+{
+    public class GoalControl : MonoBehaviour {
+
+        public int scoresTo;
+
+        private GameControl _control;
+
+        // Use this for initialization
+        void Start() {
+            _control = GameControl.Instance;
+        }
+
+        // Update is called once per frame
+        void Update() {
+
+        }
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+        }
+
+        void OnTriggerEnter2D(Collider2D collider)
+        {
+            if (collider.tag == TagControl.Ball)
+            {
+                _control.GoalScored(scoresTo);
+                Destroy(collider.gameObject, 2);
+            }
+        }
+    }
+}
